e64c3ba34cf717ac4d015ab564ecaf227e68cee6
krishna-garikapati <w0502117@campus.nscc.ca>
2024-10-21 14:42:34 -0300

    Ready to Mark

--- A2-P3-AutoInsurance.py
+++ A2-P3-AutoInsurance.py
@@ -1,19 +1,33 @@
 #Program 3 – Auto Insurance
 #Write a program that computes monthly insurance according to the provided schedule. 
 
-#Student #:     
-#Student Name:  
+#Student #:  W0502117   
+#Student Name:  Kishna Priyanka Garikapati
 
 def main():
-    # YOUR CODE STARTS HERE, each line must be indented (one tab)
-
-
-
-
-
-
-
-
-    # YOUR CODE ENDS HERE
-
+    #Three inputs
+    Gender=input("Are you 'Male' or 'Female': ")
+    Age=int(input("Enter your age: "))
+    VehiclePrice=float(input("Enter the purchase price of the vehicle: "))
+    #Conditions based on gender
+    if Gender.lower()=="male":
+        if 25>Age>=15:
+            insurance=(0.25/12)*VehiclePrice
+        elif 25<=Age<40:
+            insurance=(0.17/12)*VehiclePrice
+        elif 40<=Age<70:
+            insurance=(0.10/12)*VehiclePrice
+        else:
+            insurance=0
+    elif Gender.lower()=="female":
+        if 25>Age>=15:
+            insurance=(0.20/12)*VehiclePrice
+        elif 25<=Age<40:
+            insurance=(0.15/12)*VehiclePrice
+        elif 40<=Age<70:
+            insurance=(0.10/12)*VehiclePrice
+        else:
+            insurance=0
+    #Output to display on console
+    print("Your monthly insurance will be ${0:.2f}".format(insurance))
 main()
